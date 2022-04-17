5a9f8cea62ca5d4cb9ea37cdf97132b1575ddd4f
olowo-coder <olowofela.olanrewaju@decagon.dev>
2022-04-17 23:19:59 +0100

    Initialization

---
+++ src/main/java/com/example/graphqlspringv1/GraphQlSpringV1Application.java
@@ -0,0 +1,13 @@
+package com.example.graphqlspringv1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraphQlSpringV1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GraphQlSpringV1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/graphqlspringv1/GraphQlSpringV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.graphqlspringv1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraphQlSpringV1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
